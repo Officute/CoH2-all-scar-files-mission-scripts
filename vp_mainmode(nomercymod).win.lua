19c90f8be3557ebd404569f05f65b721be4ba6bf
Officute <panosmaga33@gmail.com>
2026-05-30 16:02:37 +0300

    Custom Win Condition .win (Options)

---
+++ vp_mainmode(nomercymod).win.lua
@@ -0,0 +1,121 @@
+name = "nm_vpc_mainmode"
+scar_file = "data:scar/winconditions/VP_mainmode.scar"
+fe_name = "NM VPC Coldnight"
+fe_priority = 0
+score_display = vp_ticker
+show_time = true
+requires_vp_ticker = true
+
+fe_option_name = "NM VPC Count:"
+options = 
+{
+	{
+		fe_name = "50 points",
+		value = 50,
+	},
+	{
+		fe_name = "75 points",
+		value = 75,
+	},
+	{
+		fe_name = "100 points",
+		value = 100,
+	},
+	{
+		fe_name = "125 points",
+		value = 125,
+	},
+	{
+		fe_name = "150 points",
+		value = 150,
+	},
+	{
+		fe_name = "175 points",
+		value = 175,
+	},
+	{
+		fe_name = "200 points",
+		value = 200,
+	},
+	{
+		fe_name = "250 points",
+		value = 250,
+	},
+	{
+		fe_name = "300 points",
+		value = 300,
+	},
+	{
+		fe_name = "350 points",
+		value = 350,
+	},
+	{
+		fe_name = "400 points",
+		value = 400,
+	},
+	{
+		fe_name = "450 points",
+		value = 450,
+	},
+	{
+		default = true,
+		fe_name = "500 points",
+		value = 500,
+	},
+	{
+		default = true,
+		fe_name = "600 points",
+		value = 600,
+	},
+	{
+		default = true,
+		fe_name = "700 points",
+		value = 700,
+	},
+	{
+		default = true,
+		fe_name = "800 points",
+		value = 800,
+	},
+	{
+		default = true,
+		fe_name = "900 points",
+		value = 900,
+	},
+	{
+		fe_name = "1000 points",
+		value = 1000,
+	},
+	{
+		fe_name = "1250 points",
+		value = 1250,
+	},
+	{
+		fe_name = "1500 points",
+		value = 1500,
+	},
+	{
+		fe_name = "1750 points",
+		value = 1750,
+	},
+	{
+		fe_name = "2000 points",
+		value = 2000,
+	},
+	{
+		fe_name = "2500 points",
+		value = 2500,
+	},
+	{
+		fe_name = "3000 points",
+		value = 3000,
+	},
+	{
+		fe_name = "4000 points",
+		value = 4000,
+	},
+	{
+		fe_name = "5000 points",
+		value = 5000,
+	},
+}
