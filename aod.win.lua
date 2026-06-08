90d2fc8c7dcc9738f80ecbf350a2d84ce9547ab6
Officute <panosmaga33@gmail.com>
2026-06-08 14:41:30 +0300

    Art of Defence (AOD) (.win file)

---
+++ aod.win.lua
@@ -0,0 +1,13 @@
+name = "art_of_defence_test"
+scar_file = "data:scar/winconditions/aod_classic.scar"
+fe_name = "Art of Defence - Classic"
+fe_priority = 0
+score_display = time
+show_time = true
+entity_replacements =
+{
+	{
+		original = "victory_point",
+		replacement = "territory_point_mp",
+	}
+}
---
+++ aod_survival.win.lua
@@ -0,0 +1,13 @@
+name = "art_of_defence_survival"
+scar_file = "data:scar/winconditions/aod_survival.scar"
+fe_name = "Art of Defence - Survival"
+fe_priority = 0
+score_display = time
+show_time = true
+entity_replacements =
+{
+	{
+		original = "victory_point",
+		replacement = "territory_point_mp",
+	}
+}
