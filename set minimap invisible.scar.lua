9ad9121a6ec4f74283d1e3778a742a2b01392836
Officute <panosmaga33@gmail.com>
2026-08-09 14:06:49 +0300

    Simple scar file

---
+++ set minimap invisible.scar.lua
@@ -0,0 +1,8 @@
+function OnInit()
+
+    EGroup_EnableMinimapIndicator(<Any Egroup name>, <false/true>)
+	SGroup_EnableMinimapIndicator(<Any Sgroup name>, <false/true>)
+
+end
+
+Scar_AddInit(OnInit)
---
+++ set selectable invulnerable.scar.lua
@@ -0,0 +1,8 @@
+function OnInit()
+
+    EGroup_SetInvulnerable(<Any Egroup name>, <false/true>)
+	SGroup_SetInvulnerable(<Any Sgroup name>, <false/true>)
+
+end
+
+Scar_AddInit(OnInit)
