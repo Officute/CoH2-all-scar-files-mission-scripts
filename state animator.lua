f34e3151900765af0d447232af3b536ea0222aeb
Officute <panosmaga33@gmail.com>
2026-08-09 14:03:42 +0300

    state animator.lua

--- state animator.lua
+++ state animator.lua
@@ -1,6 +1,7 @@
 function OnInit()
 
 	EGroup_SetAnimatorState(<Any Egroup name>, "<Any State Animator>", "<Any trigger animator>")
+	SGroup_SetAnimatorState(<Any Sgroup name>, "<Any State Animator>", "<Any trigger animator>")
 
 end
 
